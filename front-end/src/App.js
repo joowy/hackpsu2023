2edf30acacf770f82096d3788c51c9aaa3dfadc0
chriswackerman <cwa5262@psu.edu>
2023-04-01 15:20:22 -0400

    react app base

---
+++ front-end/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AI-driven Financial Inclusion</h1>
+      </header>
+      <main>
+        {/* Add your main content components here */}
+      </main>
+      <footer>
+        <p>&copy; InclusiScore. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
