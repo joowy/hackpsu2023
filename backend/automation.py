b73964e4af5d79872cad1d779ad078efba265239
David Rohweder <david01rohweder@gmail.com>
2023-04-02 00:27:41 -0400

    work done

--- backend/automation.py
+++ backend/automation.py
@@ -27,15 +27,24 @@ def update_approval_status(message):
     data = message["data"]
     print(data)
 
-    if event == 'put' and data and not path == "/":
-        #user_email = next(iter(data)) # get dict, dont know user_email to access dict
-        #user_data = data[user_email]
-
+    # DATA POSTED WHEN BACKEND WAS RUNNING
+    if event == 'put' and data and not path == "/": 
         predicted_credit_score = predict_credit_score(data)
         print(predicted_credit_score)
         data['credit_score'] = predicted_credit_score
         n_path = path.split("/")
         user_email = n_path[-1]
         db.child("users").child(user_email).update(data)
+
+    # DATA POSTED WHEN BACKEND WAS OFF --> gets a backlogged list { u1: {}, u1: { },...}
+    if event == 'put' and data and path == "/":
+        for user in data:
+            user_data = data[user]
+
+            predicted_credit_score = predict_credit_score(user_data)
+            print(predicted_credit_score)
+            user_data['credit_score'] = predicted_credit_score
+            data[user] = user_data
+            db.child("users").child(user).update(data[user])
 
 db.child("users").stream(update_approval_status,is_async=True) # listen for new post in the firebase table
